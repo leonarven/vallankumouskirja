78c5d4fd960b3dfb95480f9a60e6e12bb8279e76
Miro Nieminen <github@miron.fi>
2026-03-20 11:37:12 +0200

    Updated main JavaScript file reference in index.html to the latest version and removed outdated CSS styles in song-view.component.ts for improved layout consistency.

--- src/ts/components/song-view.component.ts
+++ src/ts/components/song-view.component.ts
@@ -45,10 +45,6 @@ let hammertime;
 			padding: 0px 1em;
 			overflow-wrap: break-word;
 		}
-		:host ::ng-deep #song-body article > header > h1,
-		:host ::ng-deep #song-body article > header > h2 {
-			display: inline-block;
-		}
 		:host ::ng-deep #song-body section {
 			text-align: left;
 			padding: 0px 1em;	
